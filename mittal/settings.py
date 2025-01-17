392eef275e6f52ac673837e52bb87656a2a11c73
Shivam Mittal <126662156+shivammittal09@users.noreply.github.com>
2025-01-17 21:56:42 +0530

    Update settings.py

--- mittal/settings.py
+++ mittal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-2l$x724=@673u2(gnmt)8ii0fc&*5t#a1m&(_73l0#j-sthtvj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
