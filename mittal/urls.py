1ba96118afe05f3c7bf8049866e03fe1d6de975e
Shivam Mittal <126662156+shivammittal09@users.noreply.github.com>
2025-01-17 22:07:04 +0530

    Update urls.py

--- mittal/urls.py
+++ mittal/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 urlpatterns = [
+    path('', lambda request: redirect('flight/welcome', permanent=True)),
     path("flight/",include("flight.urls")),
     path("admin/", admin.site.urls),
 ]
